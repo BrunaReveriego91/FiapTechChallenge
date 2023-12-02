d1affb36ddc549a4ce85bf7fac9645cf8ba9228a
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2023-12-02 12:29:03 -0300

    criacao classe investimento teste unitario

---
+++ FiapTechChallengeTests/UnitTests/InvestimentoServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FiapTechChallenge.API.Entity;
+using FiapTechChallenge.API.Interfaces.Repository;
+using FiapTechChallenge.API.Services;
+using Moq;
+using Xunit;
+
+namespace FiapTechChallenge.API.Tests.UnitTests
+{
+    public class InvestimentoServiceTests
+    {
+        Mock<IInvestimentoRepository> _investimentoRepository = new Mock<IInvestimentoRepository>();
+
+        private InvestimentoService CriarServico()
+        {
+            return new InvestimentoService(_investimentoRepository.Object);
+        }
+
+        [Fact]
+        public async Task ObterListaInvestimentosDisponiveis_DeveRetornarListaInvestimentos()
+        {
+            //Arrange
+            var servico = CriarServico();
+
+            var listaInvestimentosMoq = new List<Investimento>
+            {
+                new Investimento
+                {
+                    Id = 1,
+                    Descricao = "Tesouro Direto",
+                    IdClasseInvestimento = 1,
+                    ClasseInvestimento = new ClasseInvestimento{ Id = 1, Descricao = "Renda Fixa"}
+                }
+            };
+
+
+            _investimentoRepository.Setup(x => x.ObterListaInvestimentosDisponiveis()).ReturnsAsync(listaInvestimentosMoq);
+            //Act
+            var response = await servico.ObterListaInvestimentosDisponiveis();
+
+            //Assert
+            Assert.NotNull(response);
+        }
+
+    }
+}
