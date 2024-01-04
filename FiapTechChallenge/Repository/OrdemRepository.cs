8b1d9878119e6574f8e0435ecc9d8e84809e87a7
Rodrigo Reis <roo_reiis@hotmail.com>
2024-01-04 17:56:48 -0300

    feat: Adicionado as novas funciones para adicionar e listar ordens

---
+++ FiapTechChallenge/Repository/OrdemRepository.cs
@@ -0,0 +1,25 @@
+﻿using FiapTechChallenge.API.Entity;
+using FiapTechChallenge.API.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapTechChallenge.API.Repository
+{
+    public class OrdemRepository : EFRepository<Ordem>, IOrdemRepository
+    {
+        public OrdemRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+        public Ordem CadastrarOrdem(string tipoOrdem, int quantidade)
+        {
+            Ordem ordemCadastrada = new Ordem { Quantidade = quantidade, TipoOrdem = tipoOrdem };
+            _context.Ordem.Add(ordemCadastrada);
+            _context.SaveChanges();
+            return ordemCadastrada;
+        }
+
+        public async Task<List<Ordem>> ListarOrdens()
+        {
+            return await _context.Ordem.ToListAsync();
+        }
+    }
+}
---
+++ FiapTechChallenge/Services/OrdemService.cs
@@ -0,0 +1,26 @@
+﻿using FiapTechChallenge.API.Entity;
+using FiapTechChallenge.API.Interfaces.Repository;
+using FiapTechChallenge.API.Interfaces.Services;
+
+namespace FiapTechChallenge.API.Services
+{
+    public class OrdemService : IOrdemService
+    {
+        private readonly IOrdemRepository _ordemRepository;
+
+        public OrdemService(IOrdemRepository ordemRepository)
+        {
+            _ordemRepository = ordemRepository;
+        }
+
+        public Ordem CadastrarOrdem(string tipoOrdem, int quantidade)
+        {
+            return _ordemRepository.CadastrarOrdem(tipoOrdem, quantidade);            
+        }
+
+        public async Task<List<Ordem>> ListarOrdens()
+        {
+            return await _ordemRepository.ListarOrdens();
+        }
+    }
+}
