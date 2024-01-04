523594993ccfef5c78f4a4f8947896ccc090cea5
Rodrigo Reis <roo_reiis@hotmail.com>
2024-01-04 17:54:58 -0300

    feat: Adicionada nova entity Ordem

---
+++ FiapTechChallenge/Entity/Ordem.cs
@@ -0,0 +1,12 @@
+﻿namespace FiapTechChallenge.API.Entity
+{
+    public class Ordem : Entidade
+    {
+        public Ordem()
+        {
+        }
+
+        public string TipoOrdem { get; set; } = string.Empty;
+        public int Quantidade { get; set; } = 0;
+    }
+}
