d53062cb047d220bd16dccbd177a306c9204a6bd
Rodrigo Reis <roo_reiis@hotmail.com>
2024-01-04 17:56:07 -0300

    feat: Adicionado as novas migrations para a tabela de ordens

---
+++ FiapTechChallenge/Migrations/20240104203646_AddOrdens.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FiapTechChallenge.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrdens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ordem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INT", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoOrdem = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Quantidade = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordem", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ordem",
+                columns: new[] { "Id", "Quantidade", "TipoOrdem" },
+                values: new object[,]
+                {
+                    { 1, 99, "A Mercado" },
+                    { 2, 49, "StartStop" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ordem");
+        }
+    }
+}
