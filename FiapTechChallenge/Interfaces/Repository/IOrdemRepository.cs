c7c1182b8960cc32b5c8f0fa7d1930a00dd19819
Rodrigo Reis <roo_reiis@hotmail.com>
2024-01-04 17:55:33 -0300

    feat: Adicionado Repository e Service para as funcionalidades das ordens

---
+++ FiapTechChallenge/Interfaces/Repository/IOrdemRepository.cs
@@ -0,0 +1,11 @@
+﻿using FiapTechChallenge.API.Entity;
+
+namespace FiapTechChallenge.API.Interfaces.Repository
+{
+    public interface IOrdemRepository
+    {
+        Ordem CadastrarOrdem(string tipoOrdem, int quantidade);
+
+        Task<List<Ordem>> ListarOrdens();
+    }
+}
---
+++ FiapTechChallenge/Interfaces/Services/IOrdemService.cs
@@ -0,0 +1,11 @@
+﻿using FiapTechChallenge.API.Entity;
+
+namespace FiapTechChallenge.API.Interfaces.Services
+{
+    public interface IOrdemService
+    {
+        Ordem CadastrarOrdem(string tipoOrdem, int quantidade);
+
+        Task<List<Ordem>> ListarOrdens();
+    }
+}
